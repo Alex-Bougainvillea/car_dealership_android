1966cd78330657a64d9bc195af4250298dfc958f
AlikLooney <alekssandr04@mail.ru>
2026-05-28 01:53:30 +0300

    chore: update agp and sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.car_dealership_android"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.car_dealership_android"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
